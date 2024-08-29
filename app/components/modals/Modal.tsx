10e3d984388c3a41ee3c4b6ff36931b4e0422455
parulianrm <parulianrmanik@gmail.com>
2024-08-29 09:32:58 +0700

    pembuatan modal

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,15 @@
+interface ModalProps{
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled: string;
+}
+const Modal = () => {
+    return (  );
+}
+ 
+export default Modal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
 
 export const metadata: Metadata = {
     title: "Nest Away",
@@ -20,7 +21,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={font.className}>
-                <Navbar />
+                <ClientOnly>
+                    <Modal />
+                    <Navbar />
+                </ClientOnly>
                 {children}
             </body>
         </html>
